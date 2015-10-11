cea78cb13085333e055cc219f1fa9b252aeddeb8
Wei En <wei2912.supp0rt@gmail.com>
2015-10-11 22:42:41 +0800

    Minor changes for clarity.

--- cmbn.cpp
+++ cmbn.cpp
@@ -32,23 +32,20 @@ bool cmbn(int k, int m, int n, std::vector<int> xs) {
     // select any element better than first k till no vacancies are left or till
     // vacancies need to be filled
     int best_choice = best_k;
-    for (int i = k; i < N - vacancies; ++i)
+    for (int i = k; i < N - vacancies && vacancies > 0; ++i)
     {
         if (xs[i] < best_choice) {
             --vacancies;
             best_choice = xs[i];
             if (best_choice <= n) return true;
-            if (vacancies == 0) return false;
         }
     }
 
-    // fill up remaining vacancies
+    // go through last few elements if there are still vacancies
     for (int i = N - vacancies; i < N; ++i) {
         --vacancies;
-        if (xs[i] < best_choice) best_choice = xs[i];
-        if (best_choice <= n) return true;
+        if (xs[i] <= n) return true;
     }
-
     return false;
 }
 
